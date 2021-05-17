071e6cf97e82e5e91e0363bbc05e2bcabdbbc833
garashenco <48588267+garashenco@users.noreply.github.com>
2021-05-17 11:48:49 +0300

    RU

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
 "use strict";
+
+var end_date = {
+  "full_year": "2021",
+  // Год
+  "month": "05",
+  // Номер месяца
+  "day": "21",
+  // День
+  "hours": "12",
+  // Час
+  "minutes": "00",
+  // Минуты
+  "seconds": "00" // Секунды
+
+};
+var counterWithDay = document.getElementById('counter2');
+var counterWithoutDay = document.getElementById("counter");
+
+function diffSubtract(date1, date2) {
+  return date2 - date1;
+}
+
+var end_date_str = "".concat(end_date.full_year, "-").concat(end_date.month, "-").concat(end_date.day, "T").concat(end_date.hours, ":").concat(end_date.minutes, ":").concat(end_date.seconds),
+    timer = setInterval(function () {
+  var now = new Date();
+  var date = new Date(end_date_str);
+  var ms_left = diffSubtract(now, date);
+
+  if (ms_left <= 0) {
+    clearInterval(timer);
+  } else {
+    var res = new Date(ms_left);
+    var timerWithoutDay = "".concat(res.getUTCHours(), ":").concat(res.getUTCMinutes(), ":").concat(res.getUTCSeconds());
+    counterWithDay.innerHTML = "".concat(res.getUTCDate() - 1, "\u0414\u041D\u0415\u0419(\u041D\u042F) ").concat(timerWithoutDay);
+    counterWithoutDay.innerHTML = timerWithoutDay;
+  }
+}, 1000);
+var rulesBlockAll = document.querySelectorAll('.rules-block');
+rulesBlockAll.forEach(function (e) {
+  var rulesTitle = e.querySelector('.rules-title');
+  var hideBlock = e.querySelector('.hide-block');
+  rulesTitle.addEventListener('click', function (e) {
+    hideBlock.classList.toggle('active');
+  });
+});
